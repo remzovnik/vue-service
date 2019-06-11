41a392fa99c4aeeba85ef05715fe33c079ae9cfc
remzov <remzovnik@gmail.com>
2019-06-11 15:52:53 +0300

    bg color change

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,59 +8,59 @@
 
 <script>
 export default {
-  name: 'Nav',
+  name: "Nav",
   props: {
     submit: Function
   },
   methods: {
     reset() {
-      this.$store.dispatch('reset');
+      this.$store.dispatch("reset");
     },
     movePrev() {
-      this.$store.dispatch('movePrev');
+      this.$store.dispatch("movePrev");
     },
     moveNext() {
-      this.$store.dispatch('moveNext');
+      this.$store.dispatch("moveNext");
     }
   }
-}
+};
 </script>
 
 <style>
-  .nav {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 0 -10px;
-  }
-  .nav > * {
-    margin: 0 10px;
-  }
-  .next-btn {
-    min-width: 200px;
-    box-sizing: border-box;
-    padding: 15px 10px;
-    background-color: #000;
-    border: none;
-    color: #fff;
-    font-size: 16px;
-  }
-  .prev-btn {
-    min-width: 200px;
-    box-sizing: border-box;
-    padding: 15px 10px;
-    background-color: #fff;
-    border: 1px solid #000;
-    color: #000;
-    font-size: 16px;
-  }
-  .reset-btn {
-    min-width: 200px;
-    box-sizing: border-box;
-    padding: 15px 10px;
-    background-color: yellow;
-    border: none;
-    color: #fff;
-    font-size: 16px;
-  }
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 -10px;
+}
+.nav > * {
+  margin: 0 10px;
+}
+.next-btn {
+  min-width: 200px;
+  box-sizing: border-box;
+  padding: 15px 10px;
+  background-color: #000;
+  border: none;
+  color: #fff;
+  font-size: 16px;
+}
+.prev-btn {
+  min-width: 200px;
+  box-sizing: border-box;
+  padding: 15px 10px;
+  background-color: #fff;
+  border: 1px solid #000;
+  color: #000;
+  font-size: 16px;
+}
+.reset-btn {
+  min-width: 200px;
+  box-sizing: border-box;
+  padding: 15px 10px;
+  background-color: lightblue;
+  border: none;
+  color: #fff;
+  font-size: 16px;
+}
 </style>
